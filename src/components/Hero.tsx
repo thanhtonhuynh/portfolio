8634e0cd3591898c5d3976fc8c35ade518bc8647
Ton Huynh <kintonhtt@gmail.com>
2025-01-04 01:31:01 -0800

    add grid background to hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,11 +4,12 @@ export function Hero() {
   return (
     <section className="relative mb-4 flex h-full min-h-[calc(100vh-4rem)] scroll-m-20 flex-col items-center justify-center">
       <div className="flex flex-col items-center lg:flex-row lg:gap-16">
-        <div className="p-4 md:p-8">
+        <div className="relative p-4 md:p-8">
           <h1 className="mb-4 text-4xl font-black md:mb-8 md:text-6xl">Hello, I'm Ton Huynh.</h1>
           <p className="text-xl font-bold md:text-2xl">
             I'm a full-stack web developer with passion to create greate experiences.
           </p>
+          <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
         </div>
 
         <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
